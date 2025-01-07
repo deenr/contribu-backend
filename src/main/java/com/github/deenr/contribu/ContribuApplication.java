3256c01cce2d3ed16f93719b73733d07651bba78
Dean Reymen <deenrayman@outlook.com>
2025-01-07 17:41:26 +0300

    chore: initial commit

---
+++ src/main/java/com/github/deenr/contribu/ContribuApplication.java
@@ -0,0 +1,13 @@
+package com.github.deenr.contribu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContribuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContribuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/deenr/contribu/ContribuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.deenr.contribu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContribuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
